94f78bc557d5957b5ce232f635bf9406f78acd13
niu ran <niuran1993@gmail.com>
2025-11-21 15:24:50 +0800

    Change base path for asset loading in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './', // Use relative paths for assets
+    base: '/Block-Puzzle/', // Use relative paths for assets
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
