972f2fb6edcae24601ebf9ab3810fbfd2e674d66
niuran <niuran1993@gmail.com>
2025-11-21 15:30:29 +0800

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    base: '/Block-Puzzle/', // Use relative paths for assets
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-    }
-});
